6c119b3293777a4857f7171fe99be35d1ee9d1e6
akaishi <galigalikun@gmail.com>
2019-06-01 09:45:57 +0900

    typo

--- nkf_test.go
+++ nkf_test.go
@@ -25,19 +25,19 @@ func TestGuess(t *testing.T) {
 }
 
 func TestGuessEnc(t *testing.T) {
-	if err := guessenv(filepath.Join("testdata", "test.euc-jp.txt")); err == nil {
+	if err := guessenc(filepath.Join("testdata", "test.euc-jp.txt")); err == nil {
 		t.Errorf("test.euc-jp.txt, UTF-8 と判定されている??")
 		return
 	}
-	if err := guessenv(filepath.Join("testdata", "test.shift-jis.txt")); err == nil {
+	if err := guessenc(filepath.Join("testdata", "test.shift-jis.txt")); err == nil {
 		t.Errorf("test.shift-jis.txt UTF-8 と判定されている??")
 		return
 	}
-	if err := guessenv(filepath.Join("testdata", "test.utf-8.txt")); err != nil {
+	if err := guessenc(filepath.Join("testdata", "test.utf-8.txt")); err != nil {
 		t.Errorf("test.utf-8.txt, err = %s", err.Error())
 		return
 	}
-	if err := guessenv(filepath.Join("testdata", "test.txt")); err != nil {
+	if err := guessenc(filepath.Join("testdata", "test.txt")); err != nil {
 		t.Errorf("test.utf-8.txt, err = %s", err.Error())
 		return
 	}
